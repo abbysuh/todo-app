3c6f145495d59c055ecee7eddc89f99df59e3059
Abby Suh <abbysuh@hotmail.com>
2018-08-01 17:21:34 -0700

    Almost finished HTML/CSS

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let getToDo = document.querySelector("#to-dos");
+let getDone = document.querySelector("#done-items");
+const toDoForm = document.forms.toDoInput;
+let newToDo = [];
+let newDone = [];
