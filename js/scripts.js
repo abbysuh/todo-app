c7cd93e955c76c5e469ea72076b899eb55b96de4
Abby Suh <abbysuh@hotmail.com>
2018-08-08 18:58:49 -0700

    Progress to present

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-let getToDo = document.querySelector("#to-dos");
-let getDone = document.querySelector("#done-items");
+let getToDo = document.querySelector("#toDos");
+let getDone = document.querySelector("#doneItems");
+let toDoText = document.querySelector("#toDoText");
 const toDoForm = document.forms.toDoInput;
-let newToDo = [];
-let newDone = [];
+const submitToDoButton = document.querySelector("#submitToDo");
+const myModal = document.querySelector("#myModal");
+const modalBackdrop = document.querySelector(".modal-backdrop");
+let toDoList = [];
+let doneList = [];
+
+function addToDo(input) {
+  toDoLi = document.createElement("li");
+  liInput = document.createElement("input");
+  liInput.setAttribute("type", "checkbox");
+  toDoLi.innerHTML = input;
+  toDoLi.classList.add("myLi");
+  getToDo.appendChild(toDoLi);
+  toDoLi.appendChild(liInput);
+};
+
+toDoForm.addEventListener("submit", (event) => {
+  console.log(event, toDoForm.toDoText.value);
+  event.preventDefault();
+  console.log(toDoForm.toDoText.value);
+  addToDo(toDoForm.toDoText.value);
+  myModal.classList.add("hide");
+  const modalBackdrop = document.querySelector(".modal-backdrop");
+  modalBackdrop.classList.add("hide");
+});
